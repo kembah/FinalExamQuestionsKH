a17cad1c5698aa1bdaf98487a147e7913926a1aa
Kemba Hall <Kembah@udel.edu>
2015-12-18 19:32:38 -0500

    .

--- src/QuestionC/ILLEGALRectangle.java
+++ src/QuestionC/ILLEGALRectangle.java
@@ -1,8 +1,14 @@
 package QuestionC;
 
-public class ILLEGALRectangle {
-	private double rectangle;
-	private int number;
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ILLEGALRectangle extends Exception{
 	
 	
 	
